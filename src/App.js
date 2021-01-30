73be9879abc4ad13277edfbf46fe79f6ec835d98
sambitsuvankar <sambitsuvankarsahoo@gmail.com>
2021-01-30 21:22:59 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import React , { Component } from 'react';  // This React library allows us to write this 'html' like syntaxes inside our javascript file.
+import { CardList } from './components/card-list/card-list.component.jsx';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 
+class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField : ''
+    };
+
+    this.handleChange = this.handleChange.bind(this); 
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(user => this.setState({ monsters: user}))
+  }
+
+  handleChange(e) {
+    this.setState( {searchField : e.target.value} );
+  }
+
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        
+        <SearchBox placeholder = 'Search Monsters' handleChange={this.handleChange} />
+
+        <CardList monsters = {filteredMonsters}/>  {/*Here (monsters = {this.state.monsters}) is a props .. And everything inside this <CardList> JSX tag will be the props.children */}
+      </div>
+    );
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; // ReactDom interacts with the virtual DOM 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
